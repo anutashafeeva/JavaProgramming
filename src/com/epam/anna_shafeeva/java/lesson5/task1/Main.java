4218a4b4710700cbfb68cf76da11a1c4b8e529b0
anna <shafeeva.anya@yandex.ru>
2018-03-24 20:52:26 +0400

    lesson 5

---
+++ src/com/epam/anna_shafeeva/java/lesson5/task1/Main.java
@@ -0,0 +1,59 @@
+package com.epam.anna_shafeeva.java.lesson5.task1;
+
+import com.epam.anna_shafeeva.java.lesson5.task1.salad.Salad;
+import com.epam.anna_shafeeva.java.lesson5.task1.vegetables.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private List<Vegetable> vegetables = new ArrayList<>();
+
+    private Beet beet = new Beet(400, "boiled");
+    private Carrot carrot = new Carrot(300, "boiled");
+    private Potato potato = new Potato(400, "boiled");
+    private Cabbage cabbage = new Cabbage(200, "conserved");
+    private Cucumber cucumber = new Cucumber(200, "conserved");
+    private Onion onion = new Onion(150, "fresh");
+    private GreenPea greenPea = new GreenPea(500, "conserved");
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    private void start() {
+
+        Salad salad = new Salad();
+
+        salad.addVegetables(vegetables, potato);
+        salad.addVegetables(vegetables, beet);
+        salad.addVegetables(vegetables, carrot);
+        salad.addVegetables(vegetables, cabbage);
+        salad.addVegetables(vegetables, cucumber);
+        salad.addVegetables(vegetables, onion);
+        salad.addVegetables(vegetables, greenPea);
+
+        salad.makeSalad(vegetables);
+        System.out.println();
+
+        System.out.println("Caloricity of salad: " + salad.getCaloricity(vegetables) + " kcal" + "\n");
+
+        salad.sortVegetablesByCalories(vegetables);
+        System.out.println("Sorted vegetables by calories:");
+        for (Vegetable vegetable : vegetables) {
+            System.out.println(vegetable.toString() + " " + vegetable.countCalories() / 1000 + " kcal");
+        }
+        System.out.println();
+
+        int startWeight = 100;
+        int finishWeight = 290;
+        salad.sortVegetablesByWeight(vegetables);
+        System.out.println("Vegetable which weight beetwin " + startWeight + " and " + finishWeight + ":");
+        for (Vegetable vegetable : vegetables) {
+            if (vegetable.weight >= startWeight && vegetable.weight <= finishWeight)
+                System.out.println(vegetable.toString() + " " + vegetable.weight);
+        }
+    }
+}
